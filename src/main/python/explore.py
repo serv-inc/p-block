5673b3a83dee9ff7be5efac136e71054a27b21da
serv-inc <serv-inc@users.noreply.github.com>
2019-03-29 16:29:19 +0100

    doc: lacked first on-disk classification

--- src/main/python/explore.py
+++ src/main/python/explore.py
@@ -39,3 +39,30 @@ model.compile(optimizer='adam',
 model.fit(cifar['data'], np.array(cifar['fine_labels']), epochs=5)
 test = unpickle("./images/cifar-100-python/test")
 test_loss, test_acc = model.evaluate(test['data'], np.array(test['fine_labels']))
+
+# classifying on-disk data, see README for challenge data, should be in images/
+train_datagen = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory(
+    'test',
+    target_size=(150, 150),
+    batch_size=32, class_mode="sparse")
+validation_generator = train_datagen.flow_from_directory(
+    'test',
+    target_size=(150, 150),
+    batch_size=32, class_mode="sparse")
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(150, 150, 3)),
+    keras.layers.Dense(180, activation=tf.nn.relu),
+    keras.layers.Dense(180, activation=tf.nn.relu),
+    keras.layers.Dense(3, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit_generator(train_generator, steps_per_epoch=400, epochs=5,
+                    validation_data=validation_generator, validation_steps=40)
