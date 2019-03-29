32a96cbedc977b01d4f9ac1423eb5d46c0ecb1b0
serv-inc <serv-inc@users.noreply.github.com>
2019-03-29 16:24:16 +0100

    refactor: had more lint

--- src/main/python/explore.py
+++ src/main/python/explore.py
@@ -1,21 +1,22 @@
 #! /usr/bin/env python2
+'''data analysis steps'''
 
-# to use tf 2.0
-import pkg_resources
-pkg_resources.require("tensorflow==2.0.0a0")
-
+### to use tf 2.0
+# import pkg_resources
+# pkg_resources.require("tensorflow==2.0.0a0")
+from PIL import Image, ImageFile
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-
-def unpickle(file):
+def unpickle(filename):
     '''load cifar images from https://www.cs.toronto.edu/~kriz/cifar.html'''
     import cPickle
-    with open(file, 'rb') as fo:
-        dict = cPickle.load(fo)
-    return dict
+    with open(filename, 'rb') as file_object:
+        data = cPickle.load(file_object)
+    return data
 
 # show single image
 cifar = unpickle("./images/cifar-100-python/train")
@@ -28,19 +29,19 @@ COW.show()
 
 # try to classify
 model = keras.Sequential([
-           keras.layers.Dense(3072, activation=tf.nn.relu),
-           keras.layers.Dense(3072, activation=tf.nn.relu),
-           keras.layers.Dense(3072, activation=tf.nn.relu),
-           keras.layers.Dense(100, activation=tf.nn.softmax)
-       ])
-model.compile(optimizer='adam', 
-                     loss='sparse_categorical_crossentropy',
-                     metrics=['accuracy'])
+    keras.layers.Dense(3072, activation=tf.nn.relu),
+    keras.layers.Dense(3072, activation=tf.nn.relu),
+    keras.layers.Dense(3072, activation=tf.nn.relu),
+    keras.layers.Dense(100, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 model.fit(cifar['data'], np.array(cifar['fine_labels']), epochs=5)
 test = unpickle("./images/cifar-100-python/test")
 test_loss, test_acc = model.evaluate(test['data'], np.array(test['fine_labels']))
 
-# classifying on-disk data, see README for challenge data, should be in images/
+# classifying on-disk data
 train_datagen = keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
