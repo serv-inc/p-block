f2ed6caae4db559c51adc05dc2df4d6e0386ec01
serv-inc <serv-inc@users.noreply.github.com>
2019-05-31 13:07:15 +0200

    feat: node lacked image loading and resizing

--- sandbox/tfjs/node.js
+++ sandbox/tfjs/node.js
@@ -1,8 +1,11 @@
 const tf = require("@tensorflow/tfjs-node")
+//const fs = require("fs")
+
+const sharp = require("sharp")
 // testing
 tf.setBackend("cpu")  
 tf.enableDebugMode()
 // end testing
 const model = tf.loadLayersModel("file:///tmp/converted_model/model.json")
-
+// sharp("/tmp/test/nopns/x91.jpg").resize(224, 224)
 
