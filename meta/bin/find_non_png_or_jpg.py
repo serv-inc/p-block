49ede893ff958a886f1186d9b878108b88fff58b
serv-inc <serv-inc@users.noreply.github.com>
2021-07-31 06:59:39 +0200

    lacked: reporting of all problematic files

--- meta/bin/find_non_png_or_jpg.py
+++ meta/bin/find_non_png_or_jpg.py
@@ -8,4 +8,3 @@ for line in a:
     b = subprocess.run(["file", line], capture_output=True, check=True)
     if "PNG" not in str(b.stdout) and "JPEG" not in str(b.stdout):
         print(line + " is not PNG/jpg")
-        break
