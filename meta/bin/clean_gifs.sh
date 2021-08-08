b9def78100d6f119a2e07142e4a7f7170a92c681
serv-inc <serv-inc@users.noreply.github.com>
2021-08-08 13:57:05 +0200

    failed: did not detect all GIFs

--- meta/bin/clean_gifs.sh
+++ meta/bin/clean_gifs.sh
@@ -1,1 +1,1 @@
-for i in $(identify * | grep "GIF image data, version 89a"| cut -d ':' -f 1); do optipng -snip $i; rm $i; done
+for i in $(identify * | grep " GIF "| cut -d ':' -f 1); do optipng -snip $i; rm $i; done
