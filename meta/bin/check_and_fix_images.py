f1edfc24e018d43c3a54d3311cbe0b5a6651cf80
serv-inc <serv-inc@users.noreply.github.com>
2021-08-08 13:56:44 +0200

    lacked: file cleanup

--- meta/bin/check_and_fix_images.py
+++ meta/bin/check_and_fix_images.py
@@ -1,28 +1,28 @@
+#! /usr/bin/env python3
 """check files"""
+import logging
 import os
 from PIL import Image
 
+logging.basicConfig(level=logging.INFO)
 
-def file_has_alpha(image_filename):
-    """@return true if file contains alpha channel"""
-    return has_alpha(Image.open(image_filename))
-
-
-def has_alpha(image):
+# https://stackoverflow.com/questions/35859140/
+def has_alpha(img):
     """@return has this image an alpha channel?"""
-    return image.mode == "RGBA"
+    return img.mode in ("RGBA", "LA") or (
+        img.mode == "P" and "transparency" in img.info
+    )
 
 
-# use image.save('newpath.png') to save to valid format
 def is_type_ok(image):
     """@return image has ok format"""
     return image.format in ["JPEG", "PNG"]
 
 
-def path_as_png(path):
+def path_as_png(path_in):
     """>>> path_as_png('./challenge2018/c273735e93b041a7.jpg')
     ./challenge2018/c273735e93b041a7.png"""
-    rest = path.split(".")[:-1]
+    rest = path_in.split(".")[:-1]
     rest.append("png")
     return ".".join(rest)
 
@@ -33,7 +33,24 @@ if __name__ == "__main__":
             path = os.path.join(root, file)
             testme = Image.open(path)
             if has_alpha(testme):
-                print("{} has alpha".format(path))
-            if not is_type_ok(testme):
-                testme.save(path_as_png(path))
+                logging.info("{} has alpha".format(path))
+
+                png = testme.convert("RGBA")
+                background = Image.new("RGBA", png.size, (255, 255, 255))
+
+                alpha_composite = Image.alpha_composite(background, png)
+                alpha_composite.save(path_as_png(path))
                 os.remove(path)
+            if not is_type_ok(testme):
+                logging.info("%s has wrong type: %s", path, testme.format)
+                testme.save(path_as_png(path))
+                try:
+                    os.remove(path)
+                except FileNotFoundError:
+                    logging.warning(
+                        "path not found for %s in %s with path %s and new path %s",
+                        file,
+                        root,
+                        path,
+                        path_as_png(path),
+                    )
